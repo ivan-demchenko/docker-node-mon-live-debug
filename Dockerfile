7de81ba0846ea09a607118d041944c012c76d408
Ivan Demchenko <raqy.style@gmail.com>
2018-12-25 15:33:18 +1100

    okay, show must go on...

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ ADD ./package.json /app/package.json
 RUN npm install
 
 EXPOSE 9229
+EXPOSE 3000
 
 ADD ./src /app/src
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-function add(a, b) {
-    return a + b;
+// content of index.js
+const http = require('http')
+const port = 3000;
+
+const requestHandler = (request, response) => {
+  debugger;
+  console.log(request.url)
+  response.end('Hello Node.js Server!')
 }
 
-console.log(add(1, 7));
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
